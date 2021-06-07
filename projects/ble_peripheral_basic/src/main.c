45170802425addbda57578c24746b5b4ea686c7c
James Chow <zhouxy.vortex@gmail.com>
2021-06-07 16:37:49 +0800

    add some comments in main.c

--- projects/ble_peripheral_basic/src/main.c
+++ projects/ble_peripheral_basic/src/main.c
@@ -12,18 +12,19 @@ static void idle_state_handle();
 
 int main(void)
 {
-    app_log_init();
-    timers_init();
-    leds_init();
-    power_management_init();
-    ble_stack_init();
-    gap_params_init();
-    gatt_init();
-    advertising_init();
-    services_init();
-    conn_params_init();
+    app_log_init();                                 // 初始化log模块
+    timers_init();                                  // 初始化APP定时器
+    leds_init();                                    // 初始化按键和指示灯
+    power_management_init();                        // 初始化电源管理
+    ble_stack_init();                               // 初始化协议栈
+    gap_params_init();                              // 初始化GAP参数
+    gatt_init();                                    // 初始化GATT
+    advertising_init();                             // 初始化广播
+    services_init();                                // 初始化QWR服务
+    conn_params_init();                             // 连接参数初始化
 
     NRF_LOG_INFO("BLE template example started.");
+    // 启动广播
     advertising_start();
 
     while (true) {
